f326b38a6853f67211f5416b1185d9533a3ef8fa
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-05-24 13:27:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+// Get references to the form elements
+var radiusInput = document.getElementById("radius");
+var volumeOutput = document.getElementById("volume");
+var calculateButton = document.getElementById("submit");
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// Function to calculate the volume of a sphere
+function volume_sphere(radius) {
+  var volume = (4/3) * Math.PI * Math.pow(radius, 3);
+  return volume.toFixed(2); // Round the volume to 2 decimal places
+}
+
+// Event listener for the form submission
+calculateButton.addEventListener("click", function(event) {
+  event.preventDefault(); // Prevent form submission
+  var radiusValue = parseFloat(radiusInput.value);
+  if (!isNaN(radiusValue)) {
+    var volume = volume_sphere(radiusValue);
+    volumeOutput.value = volume;
+  }
+});
+
